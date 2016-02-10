802c7f11e3f640798faff24cb0b5c6cda3fa9386
Aid Idrizovic <idrizovicaid@gmail.com>
2016-02-10 12:07:28 -0600

    simplified project

--- server.go
+++ server.go
@@ -1,90 +1,55 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
+	"github.com/taotetek/gogopher"
 	"log"
 	"net"
-	"io"
-	"os"
 	"path/filepath"
-	"sort"
-	"strings"
+	"flag"
 )
 
-var root = flag.String("root", "", "root directory")
-var dir string
-type fileName []os.FileInfo
+const (
+		host = "localhost"
+)
 
-func suffix(f *os.FileInfo) string {
-	if f.IsDirectory() {
-		return "1"
-	}
-	n := f.Name
-	switch {
-	case strings.HasPrefix(n, ".html"):
-		return "h"
-    case strings.HasPrefix(n, ".mp3"),
-        strings.HasPrefix(n, ".aiff"),
-        string.HasPrefix(n, ".au"):
-        return "s"
-	case strings.HasPrefix(n, ".txt"),
-        strings.HasPrefix(n, ".json"),
-        strings.HasPrefix(n, ".md"):
-		return "0"
-	case strings.HasPrefix(n, ".gif"):
-		return "g"
-	case strings.HasPrefix(n, ".png"),
-		strings.HasPrefix(n, ".jpg"),
-		strings.HasPrefix(n, ".jpeg"):
-		return "I"
-	}
-	return "9"
+var (
+	port = flag.String("port", "8070", "port")
+	root = flag.String("root", "/srv/gopher", "root")
+)
+
+func init() {
+	flag.Parse()
 }
 
-func getDir() string {
-	gr := os.Getenv("GOROOT")
-	return filepath.Join(gr, "doc")
+func handle(c net.Conn) {
+
+	b := make([]byte, 1024)
+	p,_ := filepath.Abs("/srv/gopher")
+	p = "gopher://" + p
+	gd, _ := gogopher.NewGopherDir(p)
+
+	for {
+		r := c.Read(b)
+		w := c.Write(gd.ToResponse())
+		c.Close()
+		break
+	}
+
+
 }
 
-func serve(n net.Conn) {
-	defer n.Close()
-	r := bufio.NewReader(n)
-	w := bufio.NewWriter(n)
-	defer w.Flush()
+func serve(h string, p string, done chan int) {
+	l := net.Listen("tcp", h + ":" + p)
 
-	l := r.ReadSlice('\n')
-	line := string(l)
-	line = filepath.Clean(line)
-	fileName := filepath.Join(dir, line)
-	f := os.Stat(fileName)
-
-	switch {
-	case f.IsDirectory():
-		e := os.Open(fileName)
-		fg := e.Readdir(-1)
-		sort.Sort(fileName(fg))
-
-    	for _, f := range fg {
-		fmt.Fprintf(w, "%s%s\r\n",suffix(&f),strings.Join([]string{
-				f.Name,line + "/" + f.Name,"127.0.0.1","70",}, "\t"))
-		}
-
-	case f.IsRegular():
-		e := os.Open(fileName)
-		io.Copy(w, e)
-
-	default:
-		log.Printf("bad file")
+	for {
+		c := l.Accept()
+		go handle(c)
 	}
+	done <- 1
 }
 
 func main() {
-	flag.Parse()
-	dir = getDir()
-	l := net.Listen("tcp", ":70")
-	for {
-		go serve(l.Accept())
-	}
+	done := make(chan int)
+	go server(host, *port, done)
+	<-done
 }
